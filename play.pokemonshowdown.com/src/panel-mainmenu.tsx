a0c60eb9e29aada0979868f6a36abc1cd179a5f3
Daniel Chen <108989218+Daniel1464@users.noreply.github.com>
2026-05-01 23:29:59 -0400

    Preact: Add tab autocomplete (#2667)

--- play.pokemonshowdown.com/src/panel-mainmenu.tsx
+++ play.pokemonshowdown.com/src/panel-mainmenu.tsx
@@ -381,9 +381,9 @@ export class MainMenuRoom extends PSRoom {
 	 * Most queries are still handled hardcoded, so this is only for certain
 	 * special queries that need a Promise.
 	 */
-	makeQuery(id: string, param?: string) {
+	makeQuery(id: string, param?: string, excludeParamFromListener?: boolean) {
 		let fullid = id;
-		if (param) fullid += ` ${toID(param)}`;
+		if (param && !excludeParamFromListener) fullid += ` ${toID(param)}`;
 		return new Promise<any>(resolve => {
 			if (!this.listeners[fullid]) {
 				this.listeners[fullid] = [];
